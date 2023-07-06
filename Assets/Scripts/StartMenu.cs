50c5249ac4988ae06617da92971f8ea5a12e51d1
alukic00 <117369568+alukic00@users.noreply.github.com>
2023-07-06 13:43:13 +0200

    Ispravljena greska u kodu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonBehaviour
+public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
